8dd90a80825e2e903141d630ad3d73418d96fc35
hariPrasadvantaku <109710862+hariPrasadvantaku@users.noreply.github.com>
2024-12-15 18:46:49 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -4,13 +4,13 @@ import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "IzaSyDBNbDig8qwGQcGKrCEFNZCOy33mv8e96w",
-    authDomain: "goaltracker-012.firebaseapp.com",
-    projectId: "goaltracker-012",
-    storageBucket: "goaltracker-012.appspot.com",
-    messagingSenderId: "205987460323",
-    appId: "1:205987460323:web:e0ff2955c8b247fe1201d6",
-    measurementId: "G-2G8ZE80Z3B"
+    apiKey: "AIzaSyBoy7ROcgrk6-Rb0L20DehdQD_4k09K7xc",
+    authDomain: "goaltracker-76.firebaseapp.com",
+    projectId: "goaltracker-76",
+    storageBucket: "goaltracker-76.appspot.com",
+    messagingSenderId: "910571995639",
+    appId: "1:910571995639:web:7dca0fd37eea22ee55aef2",
+    measurementId: "G-E6QZ0FG1NY"
   };
 
 // Initialize Firebase
@@ -25,7 +25,7 @@ document.getElementById('signup-form').addEventListener('submit', async function
     const name = document.querySelector('input[name="name"]').value.trim();
     const email = document.querySelector('input[name="email"]').value.trim();
     const password = document.querySelector('input[name="password"]').value;
-    // const confirmPassword = document.querySelector('input[name="confirm_password"]').value;
+    const confirmPassword = document.querySelector('input[name="confirm_password"]').value;
     // const phoneNumber = document.querySelector('input[name="phoneNumber"]').value.trim();
 
     // Input validation
@@ -71,10 +71,10 @@ document.getElementById('signup-form').addEventListener('submit', async function
         return;
     }
 
-    // `if (password !== confirmPassword) {
-    //     alert("Passwords do not match!");
-    //     return;
-    // }`
+    `if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return;
+    }`
 
     try {
         // Create user with Firebase Authentication
